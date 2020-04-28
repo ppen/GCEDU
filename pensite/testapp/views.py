d91dba2f5b5cde4f6bdde4b044a9232b7e9c45dc
ppen <ppen5@yahoo.com>
2020-04-29 00:27:36 +0700

    pen project first draft

---
+++ pensite/testapp/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render, redirect, HttpResponse
+
+# Create your views here.
+
+
+def index(request):
+    """
+    Homepage
+    """
+    return HttpResponse('WEBSITE')
